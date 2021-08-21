60d8b2c928dbb5a1ac150e161234cc36592afb73
Roberto Focke <robertofocke@users.noreply.github.com>
2021-08-20 22:48:52 -0300

    script en python que usa shodan y nuclei
    script en python que usa shodan y nuclei para extraer targets que fueron vulnerados por ciberdelicuentes

---
+++ webshellhafnium.py
@@ -0,0 +1,13 @@
+import shodan
+import sys
+import os
+
+if len(sys.argv)<2:
+	print ("webshellhafnium.py apishodan country:ar ")
+	sys.exit()
+
+api=shodan.Shodan(sys.argv[1])
+result=api.search("http.title:outlook exchange port:443 "+sys.argv[2])
+for item in result["matches"]:
+	print(item["ip_str"])
+	os.system("echo https://"+item["ip_str"]+" | nuclei -t hafnium_detect.yaml -v")
